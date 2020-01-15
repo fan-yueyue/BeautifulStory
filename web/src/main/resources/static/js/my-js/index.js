a958d5d34d9f08024871404689579727b080aa12
Lwxiu <735273747@qq.com>
2020-01-15 11:57:36 +0800

    Merge branch 'master' of https://github.com/fan-yueyue/BeautifulStory
     Conflicts:
    	web/pom.xml
    	web/src/main/java/com/mr/web/WebApplication.java
    	web/src/main/resources/static/js/my-js/index.js
    	web/src/main/resources/templates/aboutus.html
    	web/src/main/resources/templates/aboutus_media.html
    	web/src/main/resources/templates/bindMobile.html
    	web/src/main/resources/templates/brandhome.html
    	web/src/main/resources/templates/brandstreet.html
    	web/src/main/resources/templates/buyerService.html
    	web/src/main/resources/templates/contactus.html
    	web/src/main/resources/templates/coupon.html
    	web/src/main/resources/templates/footprint.html
    	web/src/main/resources/templates/helpCommon.html
    	web/src/main/resources/templates/helpcenterdetail.html
    	web/src/main/resources/templates/loginEqm.html
    	web/src/main/resources/templates/mylike.html
    	web/src/main/resources/templates/mylikestore.html
    	web/src/main/resources/templates/noviceGuide.html
    	web/src/main/resources/templates/orderlist.html
    	web/src/main/resources/templates/payPassword.html
    	web/src/main/resources/templates/ruleCenter.html
    	web/src/main/resources/templates/searchgoods.html
    	web/src/main/resources/templates/selectdetail.html
    	web/src/main/resources/templates/selectindex.html
    	web/src/main/resources/templates/serviceEnsure.html
    	web/src/main/resources/templates/setAvatar.html
    	web/src/main/resources/templates/setPassword.html
    	web/src/main/resources/templates/shopdetail.html
    	web/src/main/resources/templates/shoppingHelp.html
    	web/src/main/resources/templates/view/index.html
    	web/src/main/resources/templates/view/list.html

--- web/src/main/resources/static/js/my-js/index.js
+++ web/src/main/resources/static/js/my-js/index.js
@@ -1,14 +1,45 @@
-// $(function(){
-//     alert("11111111");
-//     $.ajax({
-//         url:"http://localhost:8087/home/test",
-//         data:{},
-//         dataType:"json",
-//         success:function (data) {
-//                 console.log(data)
-//
-//         },error:function () {
-//          alert("失败");
-//         }
-//     })
-// })
+$(function(){
+    $("#pinPai").click(function () {
+        window.location.href="/web/toBrandStreet";
+    })
+
+})
+
+function loadLunBo(){
+    $.ajax({
+        url:"http://localhost:8087/home/getCarousel",
+        data:{},
+        dataType:"json",
+        success:function (data) {
+
+            if(data.code==200){
+                var html="";
+                for(var i =0 ;i<data.dataList.length;i++){
+
+
+                    html+='   <li id="slide-1" class="slide">'
+                    html+=' <img src="'+data.dataList[i].bigPic+'" alt="img1" />'
+                    html+='</li>'
+                    html+=' <ul class="nav-dots">'
+
+                    html+='   <li class="dot"><a href="#"'+data.dataList[i].bigPic+'"></a></li>'
+
+                    html+='  </ul>'
+                }
+                html+='<ul class="nav-arrows">\n' +
+                    '    <li class="arrow-left"></li>\n' +
+                    '    <li class="arrow-right"></li>\n' +
+                    '  </ul>'
+                $('#lunBo').html(html)
+
+                console.log(html);
+
+            }
+
+
+
+        },error:function () {
+            alert("失败");
+        }
+    })
+}
